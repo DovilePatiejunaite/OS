b77139571e3bbeeafb5fa33359c5bc72da2831e2
Paulekna <pauliusm612@gmail.com>
2018-03-25 18:50:43 +0300

    Pridedama klasė Memory.

---
+++ src/Memory.java
@@ -0,0 +1,13 @@
+public class Memory {
+    private double[][] array = new double[800][8];
+
+    //Konstruktorius
+    public Memory(){
+        System.out.print("Inicializuojama atmintis");
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = 0;
+            }
+        }
+    }
+}
