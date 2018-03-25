9be66fd883535b9a363049f15de86ab43f0bb98c
Paulekna <pauliusm612@gmail.com>
2018-03-25 19:02:56 +0300

    Pridedama klasė Memory.

--- src/Memory.java
+++ src/Memory.java
@@ -7,6 +7,8 @@ public class Memory {
         for (int i = 0; i < array.length; i++){
             for (int j = 0; j < array[0].length; j++) {
                 array[i][j] = 0;
+                //komentaras
+
             }
         }
     }
