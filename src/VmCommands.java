adea36e8979700027ef927ded4cd87ac1657a18f
Paulekna <pauliusm612@gmail.com>
2018-04-01 17:23:10 +0300

    Adresai VM komandose

--- src/VmCommands.java
+++ src/VmCommands.java
@@ -26,7 +26,10 @@ public class VmCommands extends Registers{
 
     //aritmetinės komandos
     //imamas dabartinis CS, nes beveik visos komandos gali būti naudojamos ir VM ir RM, kuriose CS registras skiriasi.
+    //ADD 1234
     public void add(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -47,7 +50,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //SUB 1234
     public void sub(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -69,7 +75,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //MUL 1234
     public void mul( int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -88,7 +97,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //DIV 1234
     public void div(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -115,7 +127,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //MOD 1234
     public void mod(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -135,8 +150,11 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //AND 1234
     //loginės komandos
     public void and(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -155,7 +173,11 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+
+    //OR  1234
     public void or(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -174,7 +196,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //XOR 1234
     public void xor( int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -193,6 +218,7 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //NOT 1234
     public void not(){
         int last = ~Integer.parseInt(getR());
         setR(String.valueOf(last));
@@ -202,8 +228,11 @@ public class VmCommands extends Registers{
         timer();
         INC("IP");
     }
+    //LOAD1234
     //Duomenims apdoroti skirtos komandos
     public void load(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             setR(i.trim());
@@ -217,7 +246,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //STOR1234
     public void store(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             m.setArrayWord(getR(),adress);
         }else{
@@ -230,7 +262,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //STOS1234
     public void storeString(Memory m, int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             m.setArrayWord(getRS(), adress);
         }else{
@@ -243,7 +278,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //LODS1234
     public void loadString( int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             setRS(i);
@@ -257,6 +295,7 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //LODJ1234
     //desinej 4 baitai
     public void loadJ(int bytes) {
         String s = getR();
@@ -267,6 +306,7 @@ public class VmCommands extends Registers{
         timer();
     }
     //kairej
+    //LODL1234
     public void loadS(int bytes) {
         String s = getR();
         String first = String.valueOf(bytes);
@@ -276,6 +316,7 @@ public class VmCommands extends Registers{
         timer();
     }
     //desinej 4 baitai
+    //LOSR1234
     public void loadStringJ(String bytes) {
         String s = getRS();
         String first = s.substring(0,4);
@@ -284,14 +325,19 @@ public class VmCommands extends Registers{
         timer();
     }
     //kairej
+    //LOSL1234
     public void loadStringS(String bytes) {
         String s = getRS();
         String second = s.substring(4,s.length());
         setRS(bytes.concat(second));
         INC("IP");
         timer();
+
     }
+    //WRTP1234
     public void writeP(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             m.setArrayWord(getP(), adress);
         }else{
@@ -304,7 +350,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //WRIP1234
     public void writeIP(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             m.setArrayWord(getIP(), adress);
         }else{
@@ -317,7 +366,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //WRSP1234
     public void writeSP(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             m.setArrayWord(getSP(), adress);
         }else{
@@ -330,7 +382,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
-    private void writeSS(int adress){
+    //WRSS1234
+    public void writeSS(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             m.setArrayWord(getSS(), adress);
         }else{
@@ -345,7 +400,10 @@ public class VmCommands extends Registers{
     }
 
     //Palyginimo komandos
+    //CPR 1234
     public void cpr(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             String i = m.getFromArray(adress);
             int value = Integer.parseInt(i.trim());
@@ -367,7 +425,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //CPS 1234
     public void cps(int adress){
+        needMemory(adress);
+        adress= checkMode(adress);
         if(checkAdress(adress)==1){
             String i = m.getFromArray(adress);
             //String i = m.getFromArray(adress);
@@ -386,7 +447,10 @@ public class VmCommands extends Registers{
         timer();
     }
     //Įvedimo/išvedimo komandos.
+    //PRIN
     public void print(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             setR(String.valueOf(adress));
             setIR(4);
@@ -400,7 +464,10 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //WRIT
     public void write(int adress) {
+        needMemory(adress);
+        adress = checkMode(adress);
         if(checkAdress(adress)==1) {
             setR(String.valueOf(adress));
             setIR(3);
@@ -414,62 +481,84 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //GO  1234
     //Valdymo perdavimo komandos
-    public void go(String adress){
-        setIP(adress);
+    public void go(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
+        setIP(String.valueOf(adress));
         timer();
     }
+    //HALT
     //vartotojo programos vykdymo pabaiga
     public void halt(){
         setIR(1);
         timer();
     }
-
+    //JE  1234
     //Sąlyginio valdymo perdavimo komandos
-    public void je(String adress){
+    public void je(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(getZF() == 1){
-            setIP(adress);
+            setIP(String.valueOf(adress));
         }
         timer();
     }
-    public void jn(String adress){
+    //JN  1234
+    public void jn(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(getZF() == 0){
-            setIP(adress);
+            setIP(String.valueOf(adress));
         }
         timer();
     }
-    public void jl(String adress){
+    //JL 1234
+    public void jl(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(getSF() == 1){
-            setIP(adress);
+            setIP(String.valueOf(adress));
         }
         timer();
     }
-    public void jg(String adress){
+    //JG  1234
+    public void jg(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(getSF() == 0){
-            setIP(adress);
+            setIP(String.valueOf(adress));
         }
         timer();
     }
-    public void jo(String adress){
+    //JO  1234
+    public void jo(int adress){
+        needMemory(adress);
+        adress = checkMode(adress);
         if(getCF() == 1){
-            setIP(adress);
+            setIP(String.valueOf(adress));
         }
         timer();
     }
-    //Eilės duomenų struktūros valdymo komandos. FIFO principas
+    //PUSH
+    //Stekas
     public void push(){
-        m.setArrayWord(getR(),Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        int adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        m.setArrayWord(getR(),adress);
         INC("SP");
         INC("IP");
         timer();
     }
-
+    //PUSS
     public void pushs(){
-        m.setArrayWord(getRS(),Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        int adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        m.setArrayWord(getRS(),adress);
         INC("SP");
         INC("IP");
         timer();
     }
+    //POP
     public void pop(){
         if(Integer.parseInt(getSP()) == 0){
             if(getMODE()==1) {
@@ -478,14 +567,20 @@ public class VmCommands extends Registers{
                 setERR("5");
             }
         }else {
-            String i = m.getFromArray(Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
+            int adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+            String i = m.getFromArray(adress);
             setR(i.trim());
+            String word = "-";
+            if(getMODE()==1){
+                word = "";
+            }
             m.setArrayWord("", Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
             DEC("SP");
             INC("IP");
             timer();
         }
     }
+    //POPS
     public void pops(){
         if(Integer.parseInt(getSP()) == 0){
             if(getMODE()==1) {
@@ -494,14 +589,21 @@ public class VmCommands extends Registers{
                 setERR("5");
             }
         }else {
-            String s = m.getFromArray(Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
-            setRS(s);
-            m.setArrayWord("", Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
+            int adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+            String i = m.getFromArray(adress);
+            setRS(i);
+            String word = "-";
+            if(getMODE()==1){
+                word = "";
+            }
+            m.setArrayWord(word, Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
             DEC("SP");
             INC("IP");
             timer();
         }
     }
+    //CLR
+    //PAKEISI KAIP REIKIA VM KAD VEIKTU IR REALIAI!!
     public void clears(){
         for(int i=700;i<800;i++){
             m.setArrayWord("        ", i);
@@ -510,13 +612,16 @@ public class VmCommands extends Registers{
         INC("IP");
         timer();
     }
+    //PUSM1234
     public void pushm(int adress){
-        String s = m.getFromArray(adress);
-        m.setArrayWord(s,Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        String i = m.getFromArray(adress);
+        m.setArrayWord(i,adress);
         INC("SP");
         INC("IP");
         timer();
     }
+    //POPM1234
     public void popm(int adress){
         if(Integer.parseInt(getSP()) == 0){
             if(getMODE()==1) {
@@ -525,23 +630,31 @@ public class VmCommands extends Registers{
                 setERR("5");
             }
         }else {
-            String s = m.getFromArray(Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
-            m.setArrayWord(s, adress);
-            m.setArrayWord("", Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
+            adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+            String i = m.getFromArray(adress);
+            m.setArrayWord(i, adress);
+            String word = "-";
+            if(getMODE()==1){
+                word = "";
+            }
+            m.setArrayWord(word, Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
             DEC("SP");
             INC("IP");
             timer();
         }
     }
+    //PUSF
     //supushina flagu registrus i eilę.
     public void pushf(){
         String whole = String.valueOf(getCF()).concat(String.valueOf(getSF()));
         String last = whole.concat(String.valueOf(getZF()));
-        m.setArrayWord(last,Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        int adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+        m.setArrayWord(last,adress);
         INC("SP");
         INC("IP");
         timer();
     }
+    //POPF
     public void popf(){
         if(Integer.parseInt(getSP()) == 0){
             if(getMODE()==1) {
@@ -550,11 +663,16 @@ public class VmCommands extends Registers{
                 setERR("5");
             }
         }else {
-            String whole = m.getFromArray(Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
+            int adress = checkMode(Integer.parseInt(getSS())+Integer.parseInt(getSP()));
+            String whole = m.getFromArray(adress);
+            String word = "-";
+            if(getMODE()==1){
+                word = "";
+            }
             int cf = Integer.parseInt(whole.substring(5, 6));
             int sf = Integer.parseInt(whole.substring(6, 7));
             int zf = Integer.parseInt(whole.substring(7, 8));
-            m.setArrayWord("", Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
+            m.setArrayWord(word, Integer.parseInt(getSS()) + Integer.parseInt(getSP()));
             setCF(cf);
             setSF(sf);
             setZF(zf);
@@ -564,26 +682,16 @@ public class VmCommands extends Registers{
         timer();
     }
     //HDD
+    //REHA1234
     public void readhard(String adress){
         setR(adress);
         setIR(6);
         INC("IP");
         timer();
     }
+    //WRHA1234
     public void writehard(){
         setIR(5);
-        INC("IP");
-        timer();
-    }
-
-    private void setmSS(String adress){
-        setSS(adress);
-        INC("IP");
-        timer();
-    }
-
-    private void setmSP(String adress){
-        setSP(adress);
         INC("IP");
         timer();
     }
@@ -601,7 +709,7 @@ public class VmCommands extends Registers{
     public int checkAdress(int adress){
         int availability = 0;
         if(getMODE()==1){
-            if(adress > 40 && adress < 700) {
+            if(adress > 60 && adress < 700) {
                 availability = 1;
             }else{
                 availability = 0;
@@ -617,5 +725,41 @@ public class VmCommands extends Registers{
             System.out.println("Neatpažinta klaida");
         }
         return availability;
+    }
+    public void commandToM(String command){
+        int adress;
+        if(getMODE()==0){
+            adress = m.realWordAdress(m.getArray(),Integer.parseInt(getPTR()),Integer.parseInt(getIP()));
+        } else {
+            adress = Integer.parseInt(getCS())+Integer.parseInt(getIP());
+        }
+        m.setArrayWord(command,adress);
+    }
+
+    public int checkMode(int adr){
+        int adress = adr;
+        if(getMODE()==0){
+            adress = m.realWordAdress(m.getArray(),Integer.parseInt(getPTR()),adr);
+        }
+        return adress;
+    }
+    public void needMemory(int adress){
+        if(getMODE()==0){
+            int ad = Integer.parseInt(getPTR())%100;
+            if(m.getFromArray(ad*10+Integer.parseInt(getIP())/10).trim().equals("-"))
+            {
+                moreMemoryForVM(adress);
+            }
+        }
+    }
+    //issikiriam vietos is vm->rm kur bus irasoma
+// IP yra virtualios mašinos (CS=0, adreso nekeičia)
+    //nėra komanda!! nedidina IP ir yra puslapiavimo dalis.
+    public void moreMemoryForVM(int ip){
+        int block =  m.findFreeSpace(m.getArray(),60,700);
+        int which_block = ip/10;
+        int ptr =  Integer.parseInt(getPTR())%100;
+        int adress = ptr*10+which_block;
+        m.setArrayWord(String.valueOf(block), adress);
     }
 }
